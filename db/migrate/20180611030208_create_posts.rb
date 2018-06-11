a7c076b9414b0e8864b0c6b94d278cd94450768a
Andrew Bao <bao.andrew@gmail.com>
2018-06-10 20:12:35 -0700

    Created database, models, and controllers, as well as activating bcrypt

---
+++ db/migrate/20180611030208_create_posts.rb
@@ -0,0 +1,14 @@
+class CreatePosts < ActiveRecord::Migration[5.2]
+  def change
+    create_table :posts do |t|
+      t.string :title
+      t.string :body
+      t.string :genre
+      t.string :style
+      t.string :platform
+      t.belongs_to :user, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
