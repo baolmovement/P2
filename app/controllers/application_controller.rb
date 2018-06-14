96df16ae596b77b72d001325efef99c34bb14577
Andrew Bao <bao.andrew@gmail.com>
2018-06-14 11:51:57 -0700

    Fixed vertical alignment and added liking authorization

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,7 +4,7 @@ class ApplicationController < ActionController::Base
   
     def current_user
       @current_user ||= User.find(session[:user_id]) if session[:user_id]
-    end
+    end 
 
     def home? 
       !!root_path
@@ -20,5 +20,6 @@ class ApplicationController < ActionController::Base
 
     def default_background_url
       "https://images.unsplash.com/photo-1507924267304-e51febff7b1c?ixlib=rb-0.3.5&s=0c0892192a76e451e259e2c904b2ea81&auto=format&fit=crop&w=2508&q=80"
-    end
+    end 
+  
   end
--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,19 +1,25 @@
 class LikesController < ApplicationController 
-  
+  before_action :authorize, only: [:create]
   def create  
-    
-    #Creates new like---------------------------------------------------------  
+    unless logged_in?
+      redirect_to new_user_path
+    end
+#Creates new like---------------------------------------------------------  
     @user = current_user.id
     @post = params[:post_id]
     likes = {user_id: @user, post_id: @post}
     @like = Like.new(likes)
     @like.save!
     
-    #Redirects to post's show page after a new like---------------------------------------------------------      
+#Redirects to post's show page after a new like---------------------------------------------------------      
     if @like.save
-      redirect_to post_path(@post)
-    end
+      redirect_to root_path
+    end 
+#-----------------------------------------------------  
   
+#-----------------------------------------------------  
+    
+#-----------------------------------------------------   
   end
 #-----------------------------------------------------  
 end
